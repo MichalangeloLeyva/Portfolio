767e49e2fefbb83be4dbd4ad64002a53d0abca50
Michalangelo <143594786+MichalangeloLeyva@users.noreply.github.com>
2023-09-12 15:54:31 -0700

    Add files via upload

---
+++ WEBSITE/js/script.js
@@ -0,0 +1,7 @@
+/* ===== Typing Animation =====*/
+var typed = new Typed(".typing",{
+    strings:["Web Designer","Shopify Expert","Web Developer","Youtuber"],
+    typeSpeed:50,
+    BackSpeed:30,
+    loop:true
+})
